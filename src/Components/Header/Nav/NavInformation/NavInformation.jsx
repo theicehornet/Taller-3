7f883f301cb34dc24c328833082c3d08f39ef09d
Alejandro <theicehornet1@gmail.com>
2024-02-03 15:16:54 -0500

    minor changes

--- src/Components/Header/Nav/NavInformation/NavInformation.jsx
+++ src/Components/Header/Nav/NavInformation/NavInformation.jsx
@@ -1,26 +1,28 @@
+
+
 export default function NavInformation() {
     const userData = localStorage.getItem("userData");
     const handleClickCerrarSession = () => {
-        localStorage.setItem("userData", null)
+        localStorage.setItem("userData", '')
     }
+
+
     return (
         <>
             {
                 userData ? (
-                    <>
-                        <li>
-                            <a ><p>Registrarse</p></a>
-                        </li>
-                        <li>
-                            <a ><p>Iniciar Session</p></a>
-                        </li>
-                    </>
+                    <li>
+                        <a onClick={handleClickCerrarSession}><p>Cerrar Session</p></a>
+                    </li >
                 ) :
-                    (
-                        <li>
-                            <a onClick={handleClickCerrarSession}><p>Cerrar Session</p></a>
-                        </li>
-                    )
+                (<>
+                    <li>
+                        <a ><p>Registrarse</p></a>
+                    </li>
+                    <li>
+                        <a ><p>Iniciar Session</p></a>
+                    </li>
+                 </>)
             }
         </>
     )
