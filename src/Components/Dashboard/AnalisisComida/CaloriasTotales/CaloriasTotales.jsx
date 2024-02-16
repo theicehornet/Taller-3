2ef48bc51c66c89c88a75270aeee520e2a87887c
Alejandro <theicehornet1@gmail.com>
2024-02-16 10:59:39 -0500

    minor changes

--- src/Components/Dashboard/AnalisisComida/CaloriasTotales/CaloriasTotales.jsx
+++ src/Components/Dashboard/AnalisisComida/CaloriasTotales/CaloriasTotales.jsx
@@ -11,5 +11,5 @@ export default function CaloriasTotales({ registrosMostrar }) {
     }, [registrosMostrar])
     
 
-    return (<p>Las calorias totales son: {caloriasTotales}</p>)
+    return (<p>Las calorias totales son: {caloriasTotales.toFixed(3)}</p>)
 }
